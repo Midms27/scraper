0bc8809b50dc0e618c4b48feb5e74d13b03a792e
Midms27 <54290331+Midms27@users.noreply.github.com>
2020-08-09 21:42:53 -0300

    Update app.js

--- page/assets/js/app.js
+++ page/assets/js/app.js
@@ -1,61 +1,26 @@
 'use strict';
 
-// Variável que utiliza um padrão de desenvolvimento chamado Factory
-// O importante, que depois que ela é chamada cria um objeto com funções que podem ser chamadas
-/**
- * Ex:
- * const myService = Service();
- * 
- * para regastar todos os dados: myService.findAll()
- * para salvar: myService.save() 
-*/
-const Service = () => {
-    const baseUrl = '/camisas';
-
-    return {
-        findAll() {
-            return fetch(baseUrl)
-            .then(res => res.json())
-        },
-        find(params) {
-            // pesquisa através de parâmetros
-            return fetch(baseUrl)
-            .then(res => res.json())
-        },
-        save(values) {
-            // TODO formulário para salvar novos itens
-            // Seria necessário utilizar o método POST para fazer salvamentos
-        }
-    }
-}
 
 function render(data) {
-    // TODO função para desenhar no HTML conteúdo recebido como parâmetro
-}
+    data.forEach(info => {
+    const HTMLDiv = document.getElementById('info')
+    const div = document.createElement('div');
+    HTMLDiv.appendChild(div);
+    div.innerHTML = `<img src='${info.imagem}' alt='Product image'/></br><p>${info.nome}</p></br><p>${info.preco},${info.field4}</p>`;
+})}    
 
-// const fetch = require('node-fetch'); // Isso é Node.js, não roda no browser deve ser removido
-// Fetch API é um recurso nativo nos browsers
+
 window.addEventListener('DOMContentLoaded', () => {
-    const appService = Service();
-    
-    appService.findAll()
-        .then(res => {
-            console.log(res);
-            // Preencher no HTML os dados para visualizar
-            render(res);
-        });
 
     const searchField = document.getElementById('search-field');
     const buttonSearch = document.getElementById('search-button');
+    
+    const dataURL = 'http://localhost:8000/data/json';
+
+    const getData = () => fetch(dataURL).then(res => res.json()).then(data => render(data));
+    getData()
 
     buttonSearch.addEventListener('click', (evt) => {
         evt.preventDefault();
-        appService.find({
-            value: searchField.value
-        }).then(res => {
-            console.log(res);
-            // após implementado é fazer algo similar ao anterior
-            render(res);
-        })
     });
 });
